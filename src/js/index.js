bcacb82aaf697cd3bff7303f0211b3c16436dbc8
eternel <eternel7@gmail.com>
2017-04-02 19:16:54 +0200

    move statics resources to static folder

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,10 @@ const PIXI = require('pixi.js');
 const Game = require( './game.js' );
 
 PIXI.loader
-  .add("background","resources/backgrounds/starry_night_sky.jpg")
-//  .add("background","resources/backgrounds/blue_sky.jpg")
-//  .add("background","resources/backgrounds/landscape.svg")
-  .add("planes","resources/ships/planes_left.png")
+  .add("background","static/resources/backgrounds/starry_night_sky.jpg")
+//  .add("background","static/resources/backgrounds/blue_sky.jpg")
+//  .add("background","static/resources/backgrounds/landscape.svg")
+  .add("planes","static/resources/ships/planes_left.png")
   .load(setup);
 
 function setup() {
